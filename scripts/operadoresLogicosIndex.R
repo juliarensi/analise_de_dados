6090882a49ccd0e0a96319079a0a143473cf9f57
juliarensi <juliarensi@gmail.com>
2023-04-28 16:12:03 -0300

    Usando operadores lógicos para localizar valores no data frame mtcars

---
+++ scripts/operadoresLogicosIndex.R
@@ -0,0 +1,5 @@
+
+# Usando operadores lógicos para indexação
+
+View(mtcars)
+mtcars$disp >= 400 & mtcars$wt <= 1 #testa se, numa dada linha, os valores na disp são maiores ou iguais a 400 OU se os valores em wt são iguais ou menores que 1
